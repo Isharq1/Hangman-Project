1fdbc42fdc35f278c04d3b25c5acaaf742897a5e
Tech-a44 <tech-a44@Tech-A44s-MacBook-Air.local>
2019-04-25 13:24:34 +0100

    finished game need to add css

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
   // create an array of words
   var animals = ['llama', 'octopus', 'elephant', 'bear', 'jerobe' , 'snake', 'canary', 'snail', 'giraffe', 'ram', 'cow', 'lion', 'alligator', 'tiger', 'monkey', 'beaver', 'zebra', 'deer', 'penguin', 'cockroach', 'chicken', 'jaguar', 'whale', 'butterfly', 'cougar', 'jellyfish', 'shark', 'kangaroo', 'mouse', 'lizard']
-  // var cars = ['ferrari', 'lamborghini', 'buggati', 'zonda', 'saleen', 'tesla', 'mercedes' ]
+  var cars = ['ferrari', 'lamborghini', 'buggati', 'zonda', 'saleen', 'tesla', 'mercedes','toyota','peugeot', 'mclaren', 'lexus', 'lancia', 'koenigsegg', 'jeep', 'honda', 'infiniti', 'jaguar', 'nissan', 'ford', 'seat', 'citreon', 'porsche', 'shelby', 'dodge', 'daewoo']
+  var keyCodes = { 65: "a", 66: "b", 67: "c", 68: "d", 69: "e", 70: "f", 71: "g", 72: "h", 73: "i", 74: "j", 75: "k", 76: "l", 77: "m", 78: "n", 79: "o", 80: "p", 81: "q", 82: "r", 83: "s", 84: "t", 85: "u", 86: "v", 87: "w", 88: "x", 89: "y",90: "z"}
 
   //variables
-  var wrongLetter = []
+  var keysPressed = [];
+  var wrongLetter = [];
   var lives;
   var underScores = [];
   var userGuess = [];
   var random;
   var winCounter = 0;
-  var loseCounter = 0;
   // var validChar =new Array("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
 
   //innerHTML changes
@@ -20,6 +21,8 @@ document.addEventListener("DOMContentLoaded", function(){
   var hangman = document.getElementById('stickman').src;
   var winCondition =document.getElementById('message');
   var overlayer = document.getElementById('overlayer');
+  // var btnAnimals = document.getElementById('catAnimals')
+  // var btnCars = document.getElementById('catCars')
 
 
   document.getElementById("medium").addEventListener("click", medium);
@@ -55,12 +58,12 @@ document.addEventListener("DOMContentLoaded", function(){
   function catAnimals() {
     var parent = overlayer.parentNode;
     parent.removeChild(overlayer)
-
+    startGame();
   }
   function catCars() {
     var parent = overlayer.parentNode;
     parent.removeChild(overlayer)
-
+    startGame2();
   }
 
   //pick a random words
@@ -74,13 +77,27 @@ document.addEventListener("DOMContentLoaded", function(){
     }
   //print underscores to puzzle
   puzzle.innerHTML = underScores.join(' ');
-  //reset
   }
+
+  function startGame2(){
+    random = cars[Math.floor( Math.random()*cars.length)];
+    console.log('random word ' +random ) ;
+    //show the lenght of the word
+    for (var i = 0; i < random.length; i++)
+    {
+      underScores.push('_');
+    }
+  //print underscores to puzzle
+  puzzle.innerHTML = underScores.join(' ');
+  }
+
+
+
   //hangman image
 
   function hangmanImage() {
     if (lives == 8) {
-      document.get.ElementById('stickman').src = '../project/images/tumblr_inline_pg0k73U7BJ1rh6ctt_500 copy 2.png';
+      document.getElementById('stickman').src = '../project/images/tumblr_inline_pg0k73U7BJ1rh6ctt_500 copy 2.png';
 
     } else if (lives == 7) {
       document.getElementById('stickman').src = '../project/images/tumblr_inline_pg0k73U7BJ1rh6ctt_500 copy 3.png';
@@ -126,60 +143,63 @@ document.addEventListener("DOMContentLoaded", function(){
   function winOrLose(){
     if (winCounter === random.length)
     {
-
       winCondition.innerHTML= 'Congratulations, you have WON!!!'
-      // if (userGuess = 1) {
-      //   location.assign('../project/index.html')
-      // }
+      document.onkeypress = function(event){
+        return false;
+      }
+
 
 
     } else if (lives === 0) {
       // alert('You have lost, the word was ' + random )
       winCondition.innerHTML= 'You have lost, the word was ' + random
       // location.assign('../project/index.html')
-
     }
   }
   //======================Main Game=========================
   //user guesses
 
-  document.onkeypress = function(event)
+  document.onkeydown = function(e)
   {
-    userGuess = event.key
-    //checks if the letter exists inside the word
-    //if it does you get a number greater then -1
-      if (random.indexOf(userGuess) > -1){
-      for (var i = 0; i < random.length; i++)
-      {
-        //for random word if the guess equals the random word then go to the underscope and replace with the word
-        if (random[i] === userGuess & lives>1)
+    userGuess = e.keyCode
+    if (userGuess > 64 && userGuess < 95){
+
+      //checks if the letter exists inside the word
+      //if it does you get a number greater then -1
+      if (random.indexOf(keyCodes[userGuess]) > -1){
+        for (var i = 0; i < random.length; i++)
         {
-            underScores[i] = userGuess;
+          //for random word if the guess equals the random word then go to the underscope and replace with the word
+          if (random[i] === keyCodes[userGuess] & lives>0)
+          {
+            underScores[i]= keyCodes[userGuess];
             winCounter++;
             console.log(underScores);
+            puzzle.innerHTML = underScores.join(' ')
             winOrLose();
 
+
+          }
         }
       }
-    }
-    //if it doesn't
+      //if it doesn't
       else if (lives>0) {
-      wrongLetter.push(userGuess)
-      //reduce life if guess is incorrect
-      if (lives > 0 ) {
-        lives--;
-        wordsUsed.innerHTML = wrongLetter.join(' ')
-        livesLost.innerHTML = lives
-      }
-      hangmanImage();
-      console.log(lives);
-      console.log(wrongLetter)
-      winOrLose();
+        wrongLetter.push(keyCodes[userGuess])
+        //reduce life if guess is incorrect
+        if (lives > 0 ) {
+          lives--;
+          wordsUsed.innerHTML = wrongLetter.join(' ')
+          livesLost.innerHTML = lives
+        }
+        hangmanImage();
+        console.log(lives);
+        console.log(wrongLetter)
+        winOrLose();
       }
 
-  //print typed letters and wrong letters
-   puzzle.innerHTML = underScores.join(' ')
+      //print typed letters and wrong letters
+    }
 
   }
-  startGame();
+  // startGame();
 })
